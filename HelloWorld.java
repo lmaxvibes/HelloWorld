af57189f15ada36a9ef106db5223489b5de2546b
MAX <lmaxvibes@gmail.com>
2019-10-31 14:51:47 +0200

    release v1

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package runners;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World ");
+		int x = 7;
+	System.out.println(x);
+		
+	}
+}
+
---
+++ Printer.java
@@ -0,0 +1,33 @@
+package runners;
+
+public class Printer {
+
+	public static void main(String[] args) {
+		String part1 = "There will be";
+		int visitors = 5;
+		String part2 = "people for dinner";
+		System.out.println(part1 + " " + "" +(visitors) + " " + part2);
+		System.out.println(part1 + " " + "" +(visitors+2) + " " + part2);
+			int x = (int)(Math.random()*11)+1;
+			int xs = (int)(Math.random()*12)+2;
+			System.out.println(x);
+			System.out.println(xs);
+			System.out.println(x+xs);
+			System.out.println((x+xs)/2);
+			System.out.println(x%10);
+			System.out.println(xs%10);
+			System.out.println((xs*x)/2);
+			
+			
+		
+			
+			
+			
+			
+			
+			
+			
+			
+	}
+
+}
